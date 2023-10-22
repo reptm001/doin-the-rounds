7ab4e2c87e3927b2aaa1bee0a059f6ac7d3f9369
reptm001 <reptm001@gmail.com>
2023-10-22 20:18:37 +0100

    First commit: Added Files

---
+++ Assets/Editor/CreatePrefab.cs
@@ -0,0 +1,98 @@
+﻿// Create a folder (right click in the Assets directory, click Create>New Folder)
+// and name it “Editor” if one doesn’t exist already. Place this script in that folder.
+
+// This script creates a new menu item Examples>Create Prefab in the main menu.
+// Use it to create Prefab(s) from the selected GameObject(s).
+// It will be placed in the root Assets folder.
+
+using System.IO;
+using UnityEngine;
+using UnityEditor;
+
+public class Example
+{
+    // Creates a new menu item 'Examples > Create Prefab' in the main menu.
+    [MenuItem("Tools/Create Prefab")]
+    static void CreatePrefab()
+    {
+        // Keep track of the currently selected GameObject(s)
+        GameObject[] objectArray = Selection.gameObjects;
+
+        // Loop through every GameObject in the array above
+        foreach (GameObject gameObject in objectArray)
+        {
+            Debug.Log(gameObject.name);
+            Debug.Log("Mesh time...");
+            MeshFilter[] mfs = gameObject.transform.GetComponentsInChildren<MeshFilter>();
+            int mSize = mfs.Length;
+            int count = 0;
+            
+            foreach (MeshFilter mf in mfs)
+            {
+                if (mf)
+                {
+                    string name = mf.name;
+                    count++;
+                    name = name.Replace(":", "_");
+                    name = name.Replace("/", "#");
+                    string savePath = "Assets/Meshes/" + name + ".asset";
+                    //AssetDatabase.CreateAsset(mf.mesh, savePath);
+                    
+                    //Debug.Log(count + "/" + mSize + " - Mesh");
+                }
+            }
+
+            
+
+            
+            Debug.Log("Terrain time...");
+            Terrain td = gameObject.transform.GetComponent<Terrain>();
+            Object[] savedTDs = Resources.LoadAll("Terrain", typeof(TerrainData));
+            Debug.Log(savedTDs.Length);
+            Debug.Log("------");
+
+            string correctedName = td.name;
+            correctedName = correctedName.Replace(":", "_");
+            correctedName = correctedName.Replace("/", "#");
+            TerrainData newTD = (TerrainData)ArrayUtility.Find(savedTDs, t => t.name == correctedName);
+
+            td.terrainData = null;
+            td.gameObject.GetComponent<TerrainCollider>().terrainData = null;
+            Debug.Log("Loaded new terrain for " + correctedName + " with " + newTD.name);
+            
+            /*
+            count = 0;
+            foreach (Terrain td in tds)
+            {
+                if (td)
+                {
+                    string name = td.name;
+                    name = name.Replace(":", "_");
+                    name = name.Replace("/", "#");
+                    string savePath = "Assets/Terrain/" + name  + ".asset";
+                    TerrainData tdData = td.terrainData;
+                    count++;
+                    //Debug.Log(count + "/" + tSize + " - Terrain");
+                }
+            }
+            */
+
+            // Set the path as within the Assets folder,
+            // and name it as the GameObject's name with the .Prefab format
+            string localPath = "Assets/" + gameObject.name + ".prefab";
+
+            // Make sure the file name is unique, in case an existing Prefab has the same name.
+            //localPath = AssetDatabase.GenerateUniqueAssetPath(localPath);
+
+            // Create the new Prefab.
+            //PrefabUtility.SaveAsPrefabAssetAndConnect(gameObject, localPath, InteractionMode.UserAction);
+        }
+    }
+
+    // Disable the menu item if no selection is in place.
+    [MenuItem("Examples/Create Prefab", true)]
+    static bool ValidateCreatePrefab()
+    {
+        return Selection.activeGameObject != null && !EditorUtility.IsPersistent(Selection.activeGameObject);
+    }
+}
---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public List<AxleInfo> axleInfos; // the information about each individual axle
+    public float maxMotorTorque; // maximum torque the motor can apply to wheel
+    public float maxSteeringAngle; // maximum steer angle the wheel can have
+
+    public List<GameObject> brakeLights;
+
+    // finds the corresponding visual wheel
+    // correctly applies the transform
+    public void ApplyLocalPositionToVisuals(WheelCollider collider)
+    {
+        if (collider.transform.childCount == 0)
+        {
+            return;
+        }
+
+        Transform visualWheel = collider.transform.GetChild(0);
+
+        Vector3 position;
+        Quaternion rotation;
+        collider.GetWorldPose(out position, out rotation);
+
+        visualWheel.transform.position = position;
+        visualWheel.transform.rotation = rotation;
+    }
+
+    public void FixedUpdate()
+    {
+        float motor = maxMotorTorque * Input.GetAxis("Vertical");
+        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+
+        if (motor < 0) foreach (GameObject brakeLight in brakeLights) brakeLight.SetActive(true);
+        else foreach (GameObject brakeLight in brakeLights) brakeLight.SetActive(false);
+
+        foreach (AxleInfo axleInfo in axleInfos)
+        {
+            if (axleInfo.steering)
+            {
+                axleInfo.leftWheel.steerAngle = steering;
+                axleInfo.rightWheel.steerAngle = steering;
+            }
+            if (axleInfo.motor)
+            {
+                axleInfo.leftWheel.motorTorque = motor;
+                axleInfo.rightWheel.motorTorque = motor;
+            }
+            ApplyLocalPositionToVisuals(axleInfo.leftWheel);
+            ApplyLocalPositionToVisuals(axleInfo.rightWheel);
+        }
+    }
+}
+
+[System.Serializable]
+public class AxleInfo
+{
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public bool motor; // is this wheel attached to motor?
+    public bool steering; // does this wheel apply steer angle?
+}
